a7f614727063e999fcfd3d280fb01bc4f5d9e513
gluca99 <g.luca99@gmail.com>
2025-10-30 11:29:45 +0000

    Removed functions for finetuning on the ap1_width e-phys feature

--- src/training/neuro/neuro_losses.py
+++ src/training/neuro/neuro_losses.py
@@ -6,8 +6,7 @@ import os
 import torch
 from typing import Tuple, Callable
 from training.neuro.extract_features import extract_features
-from training.neuro.differentiable_ap1width import compute_predicted_and_true_ap1_width
-from training.neuro.differentiable_sagamplitude import compute_differentiable_sag_amplitude, compute_true_sag_amplitude
+from training.neuro.differentiable_sagamplitude import compute_differentiable_sag_amplitude
 import datetime
 import matplotlib.pyplot as plt
 
@@ -15,17 +14,6 @@ def _call_with_valid_kwargs(fn: Callable, **kwargs):
     sig = inspect.signature(fn).parameters
     usable = {k: v for k, v in kwargs.items() if k in sig}
     return fn(**usable)
-
-
-def ap1_width_loss(stimulus: torch.Tensor, voltage_pred: torch.Tensor, voltage_true: torch.Tensor, loss_fn: Callable, time: torch.Tensor, dt: float, ds: int) -> torch.Tensor:
-    """
-    Compute loss between predicted and true AP1 widths.
-    
-    """
-    predicted_widths, true_widths = compute_predicted_and_true_ap1_width(stimulus, voltage_pred * 1000, voltage_true * 1000, time, dt, ds)
-
-    return loss_fn(predicted_widths, true_widths)
-
 
 def sag_amplitude_loss(stimulus: torch.Tensor, voltage_pred: torch.Tensor, voltage_true: torch.Tensor, loss_fn: Callable, time: torch.Tensor, dt: float, ds: int) -> torch.Tensor:
     """
@@ -41,10 +29,9 @@ def compute_feature_loss(stimulus: torch.Tensor, output_pred: torch.Tensor, outp
     """
     Compute loss for a given electrophysiological feature.
     """
-    valid_features = ['AP1_width', 'sag_amplitude']
+    valid_features = ['sag_amplitude']
 
     FEATURE_FUNCS = {
-        "AP1_width": ap1_width_loss,
         "sag_amplitude": sag_amplitude_loss,
     }
 
